4b8f2df1e93a68f58097f398885043a241237a18
Kush Gupta <130306801+Kushgupta10@users.noreply.github.com>
2025-06-28 23:51:18 +0530

    Update socket.js

--- Backend/socket.js
+++ Backend/socket.js
@@ -6,10 +6,10 @@ let io;
 
 function initializeSocket(server) {
     io = socketIo(server, {
-        cors: {
-            origin: '*',
-            methods: [ 'GET', 'POST' ]
-        }
+      cors: {
+  origin: "https://taki-uber-clone.vercel.app",
+  methods: ["GET", "POST"]
+}
     });
 
     io.on('connection', (socket) => {
