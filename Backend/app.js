8797395417129e940f9eccd35e24eab50a71486a
Kush Gupta <130306801+Kushgupta10@users.noreply.github.com>
2025-06-28 22:43:20 +0530

    Update app.js

--- Backend/app.js
+++ Backend/app.js
@@ -13,7 +13,13 @@ const rideRoutes = require('./routes/ride.routes');
 
 connectToDb();
 
-app.use(cors());
+const corsOptions = {
+    origin: process.env.FRONTEND_URL,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
 app.use(cookieParser());
